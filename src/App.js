15cec031dd7a2f03b0c5d1a206e50371de6ec1d8
Mak4rius <rabotiaev.makar.inc@gmail.com>
2022-10-27 23:25:37 +0300

    feat: updated react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
 import MapComponent from "./components/MapComponent";
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
 function App() {
   return (
-	<MapComponent/>
+	<Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid xs={8}>
+			<MapComponent/>
+        </Grid>
+		<Grid xs={8}>
+			<MapComponent/>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
